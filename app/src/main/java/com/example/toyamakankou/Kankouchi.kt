2487f2ea76d94343c3b1f283243bf19e5f0c0b34
akumaru1 <naimakmal1@gmail.com>
2023-12-08 18:27:32 +0900

    add navigation bar

--- app/src/main/java/com/example/toyamakankou/Kankouchi.kt
+++ app/src/main/java/com/example/toyamakankou/Kankouchi.kt
@@ -1,11 +1,31 @@
 package com.example.toyamakankou
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Kankouchi : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kankouchi)
+
+        bottomNavigationView = findViewById(R.id.BottomNavigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    startActivity(Intent(this@Kankouchi, MainActivity::class.java))
+                    true
+                }
+                R.id.settings -> {
+                    startActivity(Intent(this@Kankouchi, Settings::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
--- app/src/main/java/com/example/toyamakankou/MainActivity.kt
+++ app/src/main/java/com/example/toyamakankou/MainActivity.kt
@@ -3,19 +3,34 @@ package com.example.toyamakankou
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         /* 画面移動のため */
-        val btnKankouchi : ImageButton = findViewById(R.id.btnKankouchi);
+        val btnKankouchi : ImageButton = findViewById(R.id.btnKankouchi)
         btnKankouchi.setOnClickListener {
-            val intent = Intent(this,Kankouchi::class.java)
+            val intent = Intent(this, Kankouchi::class.java)
             startActivity(intent)
+        }
+
+        bottomNavigationView = findViewById(R.id.BottomNavigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.settings -> {
+                    startActivity(Intent(this@MainActivity, Settings::class.java))
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
